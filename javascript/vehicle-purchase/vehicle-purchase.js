3efed676284816505cedcd01372a7da89128f3b9
George Peralta <george.dias@meta.com.br>
2022-10-28 17:32:45 -0300

    exercise vehicle purchase

--- javascript/vehicle-purchase/vehicle-purchase.js
+++ javascript/vehicle-purchase/vehicle-purchase.js
@@ -25,7 +25,7 @@ export function needsLicense(kind) {
 export function chooseVehicle(option1, option2) {
   return `${
     option1 < option2 ? option1 : option2
-  } tis clearly the better choice.`;
+  } is clearly the better choice.`;
 }
 
 /**
